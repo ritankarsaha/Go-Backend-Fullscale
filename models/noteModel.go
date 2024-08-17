e23d948bd2c39d9145cd5db7e6eaa7f3800ea76a
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-17 22:12:45 +0530

    Models part 2

--- models/noteModel.go
+++ models/noteModel.go
@@ -1,18 +1,15 @@
 package models
 
-import(
-
+import (
 	"time"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Note struct{
-
-	ID 			primitive.ObjectID			`bson:"_id"`
-	Text 		string						`json:"text"`
-	Title 		string						`json:"title"`
-	Created_at  time.Time					`json:"created_at"`
-	Updated_at  time.Time					`json:"updated_at"`
-	Note_id		string						`json:"note_id"`
-
+type Note struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Text       string             `json:"text" validate:"required"`
+	Title      string             `json:"title" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Note_id    string             `json:"note_id"`
 }

fd975fdcba18b9fd5c3bc54a0176f876c0a498eb
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-17 05:31:22 +0530

    setting up the main.go file and routing which are already pre planned

--- main.go
+++ main.go
@@ -2,5 +2,40 @@ package main
 
 import(
 	"os"
-	""
+	"github.com/ritankarsaha/Go-Backend-Fullscale/database"
+	routes "github.com/ritankarsaha/Go-Backend-Fullscale/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+func main(){
+
+
+	// setting up the port on whoch the server is to be exposed.
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port = "8000"
+	}
+
+
+	//setting up the gin router
+	router := gin.New()
+	router.Use(gin.Logger())
+
+
+
+	//defining all the routes - pre-planned
+	routes.UserRoutes(router)
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}

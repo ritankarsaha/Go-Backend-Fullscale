f481397c3f0c833cf79aaa211b87768018925a3c
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-22 22:40:05 +0530

    Get a Single User Controller.

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -68,3 +68,20 @@ func GetUsers() gin.HandlerFunc{
 
 	}
 }
+
+func GetUser() gin.HandlerFunc{
+
+	return func(c *gin.Context){
+		var ctx,cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		userId := c.Param("user_id")
+		var user models.User
+		err := userCollection.FindOne(ctx,bson.M{"user_id":userId}).Decode(&user)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "An error has occured while fetching these items"})
+
+		}
+		c.JSON(http.StatusOK,user)
+	}
+}
+
